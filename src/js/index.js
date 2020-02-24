0c7046f0fb5003bc2ddea27f66f5b051eb1b4b3f
ViggieSmalls <victormovileanu@gmail.com>
2020-02-24 20:59:31 +0100

    138. How ES6 modules work

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} and x is ${x}`);
+import str from './models/Search';
+
+import { ID } from "./views/searchView";
+
+import * as searchView from "./views/searchView";
+
+console.log(`using imported functions ${searchView.add(ID, 2)} and ${searchView.multiply(3, 5)}`);
